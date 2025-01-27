ae35ea8fe2ee443268d88fc74523302d8c315f65
Zaibbee69 <jahanzaib12569@gmail.com>
2025-01-27 23:20:18 +0500

    Finished

--- index.js
+++ index.js
@@ -1,39 +1,49 @@
 // Getting the container element
-const container  = document.querySelector('.container');
+const container = document.querySelector('.container');
 const numberOfGrid = document.querySelector(".number-of-grid");
 
 const CONTAINER_SIZE = 500;
 
+createGrid();
+
+numberOfGrid.addEventListener("click", () => {
+    let gridNumber = prompt("What's the Number Of Grid? Choose Between 1 to 100");
+    if (gridNumber < 1 || gridNumber > 100 || isNaN(gridNumber)) {
+        alert("Please enter a valid number between 1 and 100.");
+        return;
+    }
+    container.innerHTML = "";
+    createGrid(gridNumber);
+});
+
 // Creating the grid
-function createGrid()
-{
-    // Setting the count of the grid
-    let gridCount = 16;
+function createGrid(gridCount = 16) {
+
+    // Resetting the Grid playground
+    container.innerHTML = "";
 
     // Setting the size of the grid
     const gridSize = CONTAINER_SIZE / gridCount;
 
-    // For the rows
-    for ( let i = 0; i < gridCount * gridCount; i ++)
-    {
-        // Ok creating an element for each grid size
+    // Creating the grid elements
+    for (let i = 0; i < gridCount * gridCount; i++) {
+        
+        // Creating an element for each grid size
         let gridElement = document.createElement('div');
 
         // Adding class to it
         gridElement.classList.add('grid-element');
 
-        // Ok so first we need to set the custom width and height of the grid element
-        gridElement.style.width = `${gridSize - 5}px`;
-        gridElement.style.height = `${gridSize - 5}px`;
+        // Setting the custom width and height of the grid element
+        gridElement.style.width = `${gridSize}px`;
+        gridElement.style.height = `${gridSize}px`;
 
         // Adding a hover property to the element
         gridElement.addEventListener("mouseover", () => {
             gridElement.style.backgroundColor = "blue";
-        })
+        });
 
-        // Now appending the child to the container
+        // Appending the element to the container
         container.appendChild(gridElement);
     }
 }
-
-createGrid();
